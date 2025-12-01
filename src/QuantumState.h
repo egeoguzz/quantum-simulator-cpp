bfd8bb52b5c7dbd3a6d2529e4560bd0d4d7f3a53
Ege Oğuz <iegeoguz@gmail.com>
2025-12-01 23:43:41 +0100

    add apply single qubit gate function

--- src/QuantumState.h
+++ src/QuantumState.h
@@ -5,6 +5,7 @@
 class QuantumState {
 public:
     QuantumState(int numQubits);
+    void applySingleQubitGate(const std::complex<double> gate[2][2], int target);
 private:
     int numQubits_;
     std::vector<std::complex<double>> state_;
